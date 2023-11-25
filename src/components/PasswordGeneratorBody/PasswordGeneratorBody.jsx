0095e7ffff79ecd86cfc3973fed881020c03e26b
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-25 19:25:55 +0100

    adding styles to container

--- src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
+++ src/components/PasswordGeneratorBody/PasswordGeneratorBody.jsx
@@ -1,5 +1,31 @@
 import s from './style.module.css'
 
 export function PasswordGeneratorBody () {
-    return <div>PasswordGeneratorBody</div>
+    return <form className={s.root}>
+        <div className={s.grid_container}> 
+        <div className={s.grid_item}>
+        SIZE
+        <select defaultValue={15}>
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
+        </select>
+        </div>
+        <div className={s.grid_item}>
+            Uppercase
+            <input type='checkbox'defaultChecked/>
+        </div>
+        <div className={s.grid_item}>
+            Numbers
+            <input type='checkbox'defaultChecked/>
+        </div>
+        <div className={s.grid_item}>
+            Specials
+            <input type='checkbox' defaultChecked/>
+        </div>
+        </div>
+        <button className={s.btn_generate}> GENERATE </button>
+    </form>
+
 }
