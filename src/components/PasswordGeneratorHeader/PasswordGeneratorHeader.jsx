fc9299792f7291dba11dd7d3bc03f489c53c9894
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-25 18:54:25 +0100

    styling lock icon

--- src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
+++ src/components/PasswordGeneratorHeader/PasswordGeneratorHeader.jsx
@@ -1,5 +1,10 @@
-import s from './style.module.css'
+import s from './style.module.css';
+import lockIcon from "../../assets/lock.svg"
 
 export function PasswordGeneratorHeader () {
-    return <div>PasswordGeneratorHeader</div>
+    return( 
+    <div className={s.root}>
+     <div className={s.title}>Password Generator</div>
+     <img src= {lockIcon} className={s.icon} alt='Lock icon' />
+    </div> )
 }
