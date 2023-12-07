2a9bc7b64a3d965df2e1f678b2f6d5b0891076fc
Franklin <71532004+fran1014@users.noreply.github.com>
2023-12-07 23:10:12 +0100

    adding shuffle event

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,3 +1,14 @@
+function shuffle (arr){
+ const array = [...arr]
+ for (let i = array.length-1; i>0; i--){
+    let j= Math.floor(Math.random()*(i+1))
+    let temp = array[i]
+    array[i]=array[j]
+    array[j]=temp
+ }
+ return array;
+}
+
 function getRandomChar (text){
 
 return text[Math.floor(Math.random()*text.lenght)];
